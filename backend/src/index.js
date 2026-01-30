a991574b600264f297ea40cf6f76f7be1f24610b
Amal Krishna Asok <amalkrishnaasok.11a1@gmail.com>
2026-01-30 20:53:28 +0530

    change2

--- backend/src/index.js
+++ backend/src/index.js
@@ -84,7 +84,7 @@ app.use("/api/stats",statsRoutes);
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(_dirname,"../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(_dirname,"../frontend","dist","index.html"));
   })
 }
